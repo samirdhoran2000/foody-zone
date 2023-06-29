50a693b41ebe5bbeda17b02408481d78673e7c21
SAMIR DHORAN <samirdhoran2000@gmail.com>
2023-06-29 15:19:13 +0530

    card is design

--- src/App.jsx
+++ src/App.jsx
@@ -44,13 +44,13 @@ const navigationArray = [
 function App() {
 
   const searchList = searchArray.map((value) => {
-   return <li key={value.id} className="px-4 py-2 rounded-lg hover:bg-zinc-700 active:bg-zinc-900 text-white">{value.name}</li>;
+   return <li key={value.id} className="px-4 py-2 rounded-lg hover:cursor-pointer hover:bg-zinc-700 active:bg-zinc-900 text-white">{value.name}</li>;
   });
   const navigationList = navigationArray.map((value) => {
    return (
      <li
        key={value.id}
-       className="px-4 py-2 rounded-lg hover:bg-zinc-700 active:bg-zinc-900 text-white"
+       className="px-4 py-2 rounded-lg hover:cursor-pointer hover:bg-zinc-700 active:bg-zinc-900 text-white"
      >
        {value.name}
      </li>
@@ -76,15 +76,27 @@ function App() {
             />
           </div>
         </TopContainer>
-        <CategoryContainer className="flex justify-center mt-2 ">
+        <CategoryContainer className="flex justify-center my-2 ">
           <ul className=" flex justify-between gap-4 bg-red-700 h-12 items-center p-6 rounded-lg">
             {searchList}
           </ul>
         </CategoryContainer>
-        <CardContainer>
-          <h3>
-            Lunch
-          </h3>
+        <CardContainer className="hover:cursor-pointer m-2">
+          <div className="w-52">
+            <img src="../public/vite.svg" alt="image" width={100} />
+          </div>
+          <div className="flex-col place-items-center h-36 ">
+            <h2 className="text-2xl font-semibold">Burger Pizza</h2>
+            <p className="mt-2 ">
+              &nbsp;&nbsp;&nbsp; Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Quia nisi rem asperiores cum?
+            </p>
+            <div className="flex  justify-end ">
+              <button className="text-md px-1 rounded-md bg-red-500">
+                Rs. 22.00
+              </button>
+          </div>
+            </div>
         </CardContainer>
       </MainContainer>
     </>
@@ -101,12 +113,14 @@ const CategoryContainer = styled.div``;
 
 const CardContainer = styled.div`
 display:flex;
-flex-direction:column;
+flex-direction:row;
 align-items:center;
-background-color:rgba(255, 255, 255, 0.281);
-
+background-color:rgba(0, 0, 0, 0.281);
+border-radius: 8px;
 min-width:300px;
 max-width:400px;
+gap: 30px;
+height: 200px;
 
 padding:10px;
 
