827d09f00c36b6daa2a7e09c67d233de05dffaed
SAMIR DHORAN <samirdhoran2000@gmail.com>
2023-06-29 21:48:57 +0530

    seperate data file of constant

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,7 @@
 import styled from "styled-components";
+import { searchArray,navigationArray  } from "./components/data";
 
-const searchArray = [
-  {
-    name: "All",
-    id: 1,
-  },
-  {
-    name: "Break Fast",
-    id: 2,
-  },
-  {
-    name: "Lunch",
-    id: 3,
-  },
-  {
-    name: "Dinner",
-    id: 4,
-  },
-  {
-    name: "Veg",
-    id: 5,
-  }
-];
 
-const navigationArray = [
-  {
-    id: 1,
-    name:'Home'
-  },
-  {
-    id: 2,
-    name:'Explore'
-  },
-  {
-    id: 3,
-    name:'About Us'
-  },
-  {
-    id: 4,
-    name:'Contact Us'
-  },
-]
 function App() {
 
   const searchList = searchArray.map((value) => {
